05d1c2a5cc4fe044fe1d610b0ddcf7dfe2553f29
Alexey Stukalov <astukalov@gmail.com>
2017-10-12 13:14:38 +0000

    enhance the return type of collect(Enumerable)
    add init_sink(Enumerable) method that defaults to Vector{eltype},
    but keeps the source type for SimpleSourceEnumerable{T,<:AbstractArray}

--- src/sink_array.jl
+++ src/sink_array.jl
@@ -1,10 +1,17 @@
+init_sink(enumerable::Enumerable) = Vector{eltype(enumerable)}()
+
+# preserve AbstractArray subtype when collecting
+# allows collecting CategoricalValue into CategoricalArray
+init_sink(enumerable::SimpleSourceEnumerable{T,<:AbstractVector{T}}) where T =
+    similar(source(enumerable), 0)
+
+# recurse to get to the root source
+init_sink(enumerable::SimpleSourceEnumerable{T,<:SimpleSourceEnumerable{T}}) where T =
+    init_sink(source(enumerable))
+
 function Base.collect(enumerable::Enumerable)
-    T = eltype(enumerable)
-    ret = Array{T}(0)
-    for i in enumerable
-        push!(ret, i)
-    end
-    return ret
+    ret = init_sink(enumerable)
+    append!(ret, enumerable)
 end
 
 function Base.collect{TS,Provider}(source::Queryable{TS,Provider})
