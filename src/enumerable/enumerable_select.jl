532f5efe44b835713c4b0ba43ec4d80372e9ac1f
David Anthoff <anthoff@berkeley.edu>
2017-11-12 19:38:44 -0800

    Fix more deprecated syntax

--- src/enumerable/enumerable_select.jl
+++ src/enumerable/enumerable_select.jl
@@ -1,15 +1,15 @@
-immutable EnumerableSelect{T, S, Q<:Function} <: Enumerable
+struct EnumerableSelect{T, S, Q<:Function} <: Enumerable
     source::S
     f::Q
 end
 
-Base.iteratorsize{T,S,Q}(::Type{EnumerableSelect{T,S,Q}}) = Base.iteratorsize(S)
+Base.iteratorsize(::Type{EnumerableSelect{T,S,Q}}) where {T,S,Q} = Base.iteratorsize(S)
 
-Base.eltype{T,S,Q}(iter::EnumerableSelect{T,S,Q}) = T
+Base.eltype(iter::EnumerableSelect{T,S,Q}) where {T,S,Q} = T
 
-Base.eltype{T,S,Q}(iter::Type{EnumerableSelect{T,S,Q}}) = T
+Base.eltype(iter::Type{EnumerableSelect{T,S,Q}}) where {T,S,Q} = T
 
-Base.length{T,S,Q}(iter::EnumerableSelect{T,S,Q}) = length(iter.source)
+Base.length(iter::EnumerableSelect{T,S,Q}) where {T,S,Q} = length(iter.source)
 
 function select(source::Enumerable, f::Function, f_expr::Expr)
     TS = eltype(source)
@@ -24,12 +24,12 @@ macro select_internal(source, f)
     :(select($(esc(source)), $(esc(f)), $(esc(q))))
 end
 
-function Base.start{T,S,Q}(iter::EnumerableSelect{T,S,Q})
+function Base.start(iter::EnumerableSelect{T,S,Q}) where {T,S,Q}
     s = start(iter.source)
     return s
 end
 
-function Base.next{T,S,Q}(iter::EnumerableSelect{T,S,Q}, s)
+function Base.next(iter::EnumerableSelect{T,S,Q}, s) where {T,S,Q}
     x = next(iter.source, s)
     v = x[1]
     s_new = x[2]
@@ -37,6 +37,6 @@ function Base.next{T,S,Q}(iter::EnumerableSelect{T,S,Q}, s)
     return v_new, s_new
 end
 
-function Base.done{T,S,Q}(iter::EnumerableSelect{T,S,Q}, state)
+function Base.done(iter::EnumerableSelect{T,S,Q}, state) where {T,S,Q}
     return done(iter.source, state)
 end
