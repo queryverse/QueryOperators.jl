75d870f302ca08531723ae2b5328c1dce059a004
Nikhil Patel <n.patel@berkeley.edu>
2018-04-18 15:26:41 -0700

    Test break statements for drop/filter; groupby

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using QueryOperators
 using Base.Test
+using NamedTuples
 
 @testset "QueryOperators" begin
 
@@ -43,6 +44,12 @@ for i in QueryOperators.drop(enum, 2)
 end
 @test dropped_str == "234"
 
+dropped_str = ""
+for i in QueryOperators.drop(enum, 80)
+    dropped_str *= string(i)
+end
+@test dropped_str == ""
+
 taken_str = ""
 for i in QueryOperators.take(enum, 2)
     taken_str *= string(i)
@@ -54,6 +61,12 @@ for i in QueryOperators.@filter(enum, x->x%2==0)
     filtered_str *= string(i)
 end
 @test filtered_str == "224"
+
+filtered_str = ""
+for i in QueryOperators.@filter(enum, x->x>100)
+    filtered_str *= string(i)
+end
+@test filtered_str == ""
 
 @test collect(QueryOperators.@filter(enum, x->x<3)) == [1,2,2]
 @test collect(QueryOperators.filter(enum, x->x<3, Expr(:dummy_expr))) == [1,2,2]
@@ -82,7 +95,6 @@ end
 
 @test errored == true
 
-
 ordered = QueryOperators.orderby(enum, x -> -x, quote x -> -x end)
 @test collect(ordered) == [4, 3, 2, 2, 1]
 
@@ -100,6 +112,18 @@ for i in ordered
     push!(orderedlist, i)
 end
 @test orderedlist == [1, 2, 2, 3, 4]
+
+
+desired = [[1], [2, 2, 3], [4]]
+grouped = QueryOperators.groupby(enum, x -> floor(x/2), quote x -> floor(x/2) end)
+@test collect(grouped) == desired
+
+g = []
+for i in grouped
+    push!(g, i)
+end
+@test g == desired
+
 
 # Show/table formatting tests -- we can only test that these don't error when called.
 #@test QueryOperators.printtable(Core.CoreSTDOUT(), enum) == nothing        # this is broken?
