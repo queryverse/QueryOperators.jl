4260e357ab197c32d3ff4e0cf71720653703c044
Nikhil Patel <n.patel@berkeley.edu>
2018-04-18 16:12:08 -0700

    Remove iterators since they aren't(?) necessary

--- test/runtests.jl
+++ test/runtests.jl
@@ -112,30 +112,14 @@ desired = [[1], [2, 2, 3], [4]]
 grouped = QueryOperators.@groupby(enum, x -> floor(x/2), x->x)
 @test collect(grouped) == desired
 
-g = []
-for i in grouped
-    push!(g, i)
-end
-@test g == desired
-
-
 outer = QueryOperators.query([1,2,3,4,5,6])
 inner = QueryOperators.query([2,3,4,5])
 
 join_desired = [[3,2], [4,3], [5,4], [6,5]]
-joined_list = []
-for i in collect(QueryOperators.@join(outer, inner, x->x, x->x+1, (i,j)->[i,j]))
-    push!(joined_list, i)
-end
-@test joined_list == join_desired
-
+@test collect(QueryOperators.@join(outer, inner, x->x, x->x+1, (i,j)->[i,j])) == join_desired
 
 group_desired = [[1, Int64[]], [2, Int64[]], [3, [2]], [4, [3]], [5, [4]], [6, [5]]]
-grouped_list = []
-for i in collect(QueryOperators.@groupjoin(outer, inner, x->x, x->x+1, (i,j)->[i,j]))
-    push!(grouped_list, i)
-end
-@test grouped_list == group_desired
+@test collect(QueryOperators.@groupjoin(outer, inner, x->x, x->x+1, (i,j)->[i,j])) == group_desired
 
 
 # Show/table formatting tests -- we can only test that these don't error when called.
