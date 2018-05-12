da6147ac6b78520ceba9442216fa22856353a486
David Anthoff <anthoff@berkeley.edu>
2018-05-11 18:49:06 -0700

    Make take iteratoreltype agnostic

--- src/enumerable/enumerable_take.jl
+++ src/enumerable/enumerable_take.jl
@@ -1,27 +1,26 @@
-struct EnumerableTake{T,S} <: Enumerable
+struct EnumerableTake{S} <: Enumerable
     source::S
     n::Int
 end
 
 function take(source::Enumerable, n::Integer)
-    T = eltype(source)
     S = typeof(source)
-    return EnumerableTake{T,S}(source, Int(n))
+    return EnumerableTake{S}(source, Int(n))
 end
 
-Base.iteratorsize(::Type{EnumerableTake{T,S}}) where {T,S} = Base.iteratorsize(S) in (Base.HasLength(), Base.HasShape()) ? Base.HasLength() : Base.SizeUnknown()
+Base.iteratorsize(::Type{EnumerableTake{S}}) where {S} = Base.iteratorsize(S) in (Base.HasLength(), Base.HasShape()) ? Base.HasLength() : Base.SizeUnknown()
 
-Base.eltype(iter::EnumerableTake{T,S}) where {T,S} = T
+Base.iteratoreltype(::Type{EnumerableTake{S}}) where {S} = Base.iteratoreltype(S)
 
-Base.eltype(::Type{EnumerableTake{T,S}}) where {T,S} = T
+Base.eltype(::Type{EnumerableTake{S}}) where {S} = eltype(S)
 
-Base.length(iter::EnumerableTake{T,S}) where {T,S} = min(length(iter.source),iter.n)
+Base.length(iter::EnumerableTake{S}) where {S} = min(length(iter.source),iter.n)
 
-function Base.start(iter::EnumerableTake{T,S}) where {T,S}
+function Base.start(iter::EnumerableTake{S}) where {S}
     return iter.n, start(iter.source)
 end
 
-function Base.next(iter::EnumerableTake{T,S}, s) where {T,S}
+function Base.next(iter::EnumerableTake{S}, s) where {S}
     n, source_state = s
     x = next(iter.source, source_state)
     v = x[1]
@@ -29,7 +28,7 @@ function Base.next(iter::EnumerableTake{T,S}, s) where {T,S}
     return v, (n-1, source_new)
 end
 
-function Base.done(iter::EnumerableTake{T,S}, state) where {T,S}
+function Base.done(iter::EnumerableTake{S}, state) where {S}
     n, source_state = state
     return n<=0 || done(iter.source, source_state)
 end
