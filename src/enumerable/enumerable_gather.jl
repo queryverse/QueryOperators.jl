f11d2df81885e562debdfd5f71c6671483d7c5f1
Tony Lian <1040424979@qq.com>
2018-11-04 11:57:11 -0800

    Add index support for gather

--- src/enumerable/enumerable_gather.jl
+++ src/enumerable/enumerable_gather.jl
@@ -1,26 +1,36 @@
-struct EnumerableGather{T,S} <: Enumerable
+struct EnumerableGather{T,S,F} <: Enumerable
     source::S
+    fields::F
+    indexField::Symbol
 end
 
-function gather(source::Enumerable)
+function gather(source::Enumerable, withIndex::Bool = false)
     T = eltype(source)
-    return EnumerableGather{T, typeof(source)}(source)
+    fields = fieldnames(T)
+    F = typeof(fields)
+    if withIndex
+        return EnumerableGather{T, typeof(source), F}(source, fields, fields[1])
+    else
+        return EnumerableGather{T, typeof(source), F}(source, fields, Symbol())
+    end
 end
 
 function Base.iterate(iter::EnumerableGather{T, S}) where {T, S}
     source = iter.source
     fields = fieldnames(T)
-    #rows = (Base.IteratorSize(typeof(source)) isa Base.HasLength || Base.IteratorSize(typeof(source)) isa Base.HasShape) ? length(source) * length(fieldnames(T)) : 0
-
     elements = Array{Any}(undef, 0)
-    # println("T:", T)
-    # println("fieldnames: ",fieldnames(T))
     for i in iter.source
-        for j in fields
-            # println("What j is:", j)
-            push!(elements, (columnName = j, value = i[j]))
+        if iter.indexField == Symbol() # without index field
+            for j in fields
+                push!(elements, (columnName = j, value = i[j]))
+            end
+        else
+            for j in fields
+                if j != iter.indexField
+                    push!(elements, (index = i[iter.indexField], columnName = j, value = i[j]))
+                end
+            end
         end
-        # println("What i is: ", i)
     end
     if length(elements) == 0
         return nothing
