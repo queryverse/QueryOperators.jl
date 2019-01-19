d87771f7e05fc50d7d18d9ebb564c446bc02a0ee
Tony Lian <1040424979@qq.com>
2019-01-19 15:53:18 -0800

    Determine T in `gather` and rename local variables

--- src/enumerable/enumerable_gather.jl
+++ src/enumerable/enumerable_gather.jl
@@ -11,40 +11,39 @@ struct Not{T}
 end
 
 function gather(source::Enumerable, args...; key::Symbol = :key, value::Symbol = :value)
-    T = eltype(source)
-    fields = fieldnames(T)
-    F = typeof(fields)
-    if args != (false,)
-        prev = ()
+    fields = fieldnames(eltype(source))
+    
+    if length(args) > 0
+        indexFields = ()
         firstArg = true
         for arg in args
             if typeof(arg) == Symbol
-                prev = (prev..., arg)
+                indexFields = (indexFields..., arg)
             else typeof(arg) == Not{Symbol}
                 if firstArg
-                    prev = (a for a in fields if a != arg.val)
+                    indexFields = (a for a in fields if a != arg.val)
                 else
-                    prev = (a for a in prev if a != arg.val)
+                    indexFields = (a for a in indexFields if a != arg.val)
                 end
             end
             firstArg = false
         end
     else
-        prev = fields
+        indexFields = fields
     end
 
-    I = typeof(prev)
-    return EnumerableGather{T, typeof(source), F, I}(source, fields, prev, key, value)
+    savedFields = (n for n in fields if !(n in indexFields)) # fields that are not in `indexFields`
+    T = NamedTuple{(key, value, savedFields...)}
+    return EnumerableGather{T, typeof(source), typeof(fields), typeof(indexFields)}(source, fields, indexFields, key, value)
 end
 
 function Base.iterate(iter::EnumerableGather{T, S, F, I}) where {T, S, F, I}
     source = iter.source
-    fields = fieldnames(T)
-    elements = Array{Any}(undef, 0)
+    elements = Array{T}(undef, 0)
     
     savedFields = (n for n in iter.fields if !(n in iter.indexFields))
     for i in iter.source
-        for j in fields
+        for j in iter.fields
             if j in iter.indexFields
                 push!(elements, NamedTuple{(iter.key, iter.value, savedFields...)}((j, i[j], Base.map(n->i[n], savedFields)...)))
             end
@@ -65,11 +64,5 @@ function Base.iterate(iter::EnumerableGather{T, S, F, I}, state) where {T, S, F,
 end
 
 function Base.eltype(iter::EnumerableGather{T, S, F, I}) where {T, S, F, I}
-    if length(iter.indexFields) == 0
-        return nothing
-    end
-    savedFields = (n for n in iter.fields if !(n in iter.indexFields))
-    firstRow = iterate(iter.source)[1]
-    firstIndex = iter.indexFields[1]
-    return NamedTuple{(iter.key, iter.value, savedFields...), typeof(((firstIndex, firstRow[firstIndex], Base.map(n->firstRow[n], savedFields)...)))}
+    return T
 end
